ea1df677bee322b53ef62d2818d6afae6153447c
Dmytro Shteflyuk <kpumuk@kpumuk.info>
2022-11-18 16:38:01 -0500

    Monitor processes indefinitely, or until the main process dies (identified by PID or PPID)

--- main.go
+++ main.go
@@ -4,16 +4,16 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/shirou/gopsutil/v3/process"
 )
 
 var pidFilter = flag.Int("pid", 0, "filter processes by process pid")
 var ppidFilter = flag.Int("ppid", 0, "filter processes by parent process pid")
+var waitDuration = flag.Duration("wait", time.Second, "how many seconds to sleep between iterations")
 
-func main() {
-	flag.Parse()
-
+func printProcessStats() {
 	processes, err := process.Processes()
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "Failed to fetch processes: %v\n", err)
@@ -46,5 +46,28 @@ func main() {
 			continue
 		}
 		fmt.Printf("%d\t%d\t%s\t%d\t%.2f\n", p.Pid, ppid, name, mem.RSS, cpu)
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	var mainPid int32
+	if *pidFilter > 0 {
+		mainPid = int32(*pidFilter)
+	} else if *ppidFilter > 0 {
+		mainPid = int32(*ppidFilter)
+	}
+
+	for {
+		if mainPid > 0 {
+			_, err := process.NewProcess(mainPid)
+			if err != nil {
+				break
+			}
+		}
+
+		printProcessStats()
+		time.Sleep(*waitDuration)
 	}
 }
