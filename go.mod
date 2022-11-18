fee5573ea797f5f88c91617ca88b753c6f432b56
Dmytro Shteflyuk <kpumuk@kpumuk.info>
2022-11-18 16:15:38 -0500

    List processes and print their pid, ppid, name, RSS memory usage, and CPU %

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/kpumuk/psbench
 
 go 1.19
+
+require github.com/shirou/gopsutil/v3 v3.22.10
+
+require (
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/lufia/plan9stats v0.0.0-20220913051719-115f729f3c8c // indirect
+	github.com/power-devops/perfstat v0.0.0-20220216144756-c35f1ee13d7c // indirect
+	github.com/tklauser/go-sysconf v0.3.11 // indirect
+	github.com/tklauser/numcpus v0.6.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.2 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/shirou/gopsutil/v3/process"
+)
+
 func main() {
-	println("Hello")
+	processes, err := process.Processes()
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "Failed to fetch processes: %v\n", err)
+		os.Exit(1)
+	}
+
+	for _, p := range processes {
+		ppid, err := p.Ppid()
+		if err != nil {
+			continue
+		}
+		name, err := p.Name()
+		if err != nil {
+			continue
+		}
+		mem, err := p.MemoryInfo()
+		if err != nil {
+			continue
+		}
+		cpu, err := p.CPUPercent()
+		if err != nil {
+			continue
+		}
+		fmt.Printf("%d\t%d\t%s\t%d\t%.2f\n", p.Pid, ppid, name, mem.RSS, cpu)
+	}
 }
